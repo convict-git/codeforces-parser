03dc06108c4b40fa0cb11660b4e25f80d695c271
Priyanshu Shrivastav <official.mr.convict@gmail.com>
2018-08-29 10:38:16 +0530

    Update parse.py
    Problem wise parsing by adding --p tag.
    Compiler flag to check unsigned-integer-overflow

--- parse.py
+++ parse.py
@@ -22,9 +22,9 @@ import argparse
 ###########################
 language_params = {
         'c++14' : {
-            'TEMPLATE'    : 'main.cc',
-            'DEBUG_FLAGS' : '-DDEBUG',
-            'COMPILE_CMD' : 'g++ -g -std=c++14 -Wall $DBG',
+            'TEMPLATE'    : '/home/convict/temp.cpp',
+            'DEBUG_FLAGS' : '-DDEBUG -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC',
+            'COMPILE_CMD' : 'g++ -g -std=gnu++14 -Wall -Wextra -pedantic -Wshadow -Wfloat-equal -Wconversion -Wshift-overflow=2 -Wduplicated-cond -D_FORTIFY_SOURCE=2 -fsanitize=address -fsanitize=signed-integer-overflow -fsanitize=bounds -O2 $DBG',
             'RUN_CMD'     : './a.out'
             },
         'go'    : {
@@ -221,31 +221,41 @@ def main():
     parser.add_argument('--language', '-l', default="c++14", help="The programming language you want to use "
             "(c++14, go)")
     parser.add_argument('contest', help="")
+    parser.add_argument('--p', default="all", help="")
     args = parser.parse_args()
 
     contest = args.contest
     language = args.language
+    p = args.p
 
-    # Find contest and problems.
-    print ('Parsing contest %s for language %s, please wait...' % (contest, language))
     content = parse_contest(contest)
     print (BOLD+GREEN_F+'*** Round name: '+content.name+' ***'+NORM)
-    print ('Found %d problems!' % (len(content.problems)))
-
-    # Find problems and test cases.
     TEMPLATE = language_params[language]["TEMPLATE"]
-    for index, problem in enumerate(content.problems):
-        print ('Downloading Problem %s: %s...' % (problem, content.problem_names[index]))
-        folder = '%s-%s/%s/' % (contest, language, problem)
+
+    if p == "all":
+        # Find contest and problems.
+        #print ('Parsing contest %s for language %s, please wait...' % (contest, language))
+        print ('Found %d problems!' % (len(content.problems)))
+
+        # Find problems and test cases.
+        for index, problem in enumerate(content.problems):
+            print ('Downloading Problem %s: %s...' % (problem, content.problem_names[index]))
+            folder = '%s/%s/' % (contest, problem)
+            call(['mkdir', '-p', folder])
+            call(['cp', '-n', TEMPLATE, '%s/%s.%s' % (folder, problem, TEMPLATE.split('.')[1])])
+            num_tests = parse_problem(folder, contest, problem)
+            print('%d sample test(s) found.' % num_tests)
+            generate_test_script(folder, language, num_tests, problem)
+            print ('========================================')
+    else :
+        print ('Downloading Problem %s: from contest %s..' % (p, str(contest)))
+        folder = '%s/%s/' % (contest, p)
         call(['mkdir', '-p', folder])
-        call(['cp', '-n', TEMPLATE, '%s/%s.%s' % (folder, problem, TEMPLATE.split('.')[1])])
-        num_tests = parse_problem(folder, contest, problem)
+        call(['cp', '-n', TEMPLATE, '%s/%s.%s' % (folder, p, TEMPLATE.split('.')[1])])
+        num_tests = parse_problem(folder, contest, p)
         print('%d sample test(s) found.' % num_tests)
-        generate_test_script(folder, language, num_tests, problem)
+        generate_test_script(folder, language, num_tests, p)
         print ('========================================')
-
-    print ('Use ./test.sh to run sample tests in each directory.')
-
 
 if __name__ == '__main__':
     main()
